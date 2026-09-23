7d1e24e14ac5a0067a50828c3d11f74d73f649a8
srirecw9 <srirecw9@gmail.com>
2026-09-23 10:00:45 +0530

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot + Docker + Kubernetes!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Kubernetes!";
+    }
+
+    @GetMapping("/employee")
+    public String employee() {
+        return "Employee: 101, Name: Srinivas";
+    }
+}
