2e8a2944369799887e83afdaac4507cfc634bcec
srirecw9-bit <srirecw9@klh.edu.in>
2026-09-25 15:27:55 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot + Docker + Kubernetes!";
+        return "Hello from Spring3 Boot + Docker + Kubernetes!";
     }
 
     @GetMapping("/hello")
